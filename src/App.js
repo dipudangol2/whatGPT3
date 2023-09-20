621dc24ef8fadde085c157723f33f19247b6236b
dipudangol2 <dipudangol2@gmail.com>
2023-09-20 14:00:48 +0545

    Complete Navbar

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import {
   Header,
 } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
+import "./App.css";
 const App = () => {
   return (
     <div className="App">
-      <div className="gradient_bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
